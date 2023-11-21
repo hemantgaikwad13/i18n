bb9cff2c08c331b249b84aa15fe34ebd8dc026b2
hemantgaikwad13 <hsgaikwad0015@gmail.com>
2023-11-21 19:34:02 +0530

    I18n implemented

---
+++ src/utilis/i18n.js
@@ -0,0 +1,13 @@
+import langaugeConfig from "./lang.json";
+const I18n= (keyword, placeholder, value)=>{
+    const lang=localStorage.getItem("lang") || "en";
+
+    const text=langaugeConfig[lang][keyword];
+    
+    if(placeholder && value){
+        return text.replace(placeholder, value);
+    }
+
+    return text;
+}
+export default I18n;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import "./Home.css"
+import "./Home.css";
+import I18n from "./../../utilis/i18n"
 
 function Home() {
+    const usersCount= 15;
   return (
     <div>
-      Home
+      <h1>{I18n("welcomeMessage")}</h1>
+
+      <p>{I18n("normalMessage")}</p>
+
+      <h3>{I18n("greetingMessage")}</h3>
+      
+      <p>{I18n("userStatMessage", "<studentCount>", usersCount)}</p>
+
+      <select
+      defaultValue={localStorage.getItem("lang")}
+      onChange={(e)=>{
+        localStorage.setItem("lang",e.target.value);
+        window.location.reload();
+      }}>
+        <option value="mr">Marathi</option>
+        <option value="hi">Hindi</option>
+        <option value="en">English</option>
+      </select>
+      
     </div>
   )
 }
